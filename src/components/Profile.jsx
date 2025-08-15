6b244fb03a69e341bfc9551cd62901a69190fb2e
meune <ekpeterechioma@gmail.com>
2025-08-15 04:52:25 -0600

    Profile section done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,13 +7,13 @@ const Profile = () => {
             <div className=' w-full mx-auto text-center max-w-md md:max-w-3xl lg:max-w-4xl '>
                 <p className='text-6xl text-[#2e4f21] tracking-tighter'>Hear From Our Happy Clients</p>
             </div>
-            <div className='flex flex-col md:flex-row justify-center md:items-start m-8 mt-19'>
-                <div className='flex flex-row px-[-5rem] md:w-[45%]  md:flex-col mr-8  '>
+            <div className='flex flex-col md:flex-row justify-center md:items-start mr-8 mt-19'>
+                <div className='flex flex-row px-[-5rem] p-5 w-90  md:flex-col mr-8 '>
                     <div className=' py-8 px-5 bg-[#2E4F21] rounded-lg text-[#FFFFFF]'>
                         <p className='text-xl'>Graphic Designer</p>
                         <p className=' text-[#7D9276] mt-2 text-sm'>Ontario, Canada</p>
                     </div>
-                     <div className=' py-8 px-5 rounded-lg text-[#2e4f21]'>
+                     <div className=' py-8 px-4 rounded-lg text-[#2e4f21]'>
                         <p className='text-xl'>Commercial Photographer</p>
                         <p className=' text-[#7D9276] mt-2 text-sm'>Ontario, Canada</p>
                     </div>
