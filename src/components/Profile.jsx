19e523d0460b6892f7f3974d135b56320696d674
meune <ekpeterechioma@gmail.com>
2025-08-15 10:34:04 -0600

    Profile section done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,9 +8,9 @@ const Profile = () => {
                 <p className='text-6xl text-[#2e4f21] tracking-tighter'>Hear From Our Happy Clients</p>
             </div>
             <div className='flex flex-col md:flex-row justify-center md:items-start mr-8 mt-19'>
-                <div className='flex flex-row px-[-5rem] p-5 w-90  md:flex-col mr-8 '>
+                <div className='flex flex-row justify-between p-5 px-auto md:flex-col md:w-90 md:mr-8  '>
                     <div className=' py-8 px-5 bg-[#2E4F21] rounded-lg text-[#FFFFFF]'>
-                        <p className='text-xl'>Graphic Designer</p>
+                        <p className='text-sm md:text-xl'>Graphic Designer</p>
                         <p className=' text-[#7D9276] mt-2 text-sm'>Ontario, Canada</p>
                     </div>
                      <div className=' py-8 px-3 rounded-lg text-[#2e4f21]'>
@@ -23,7 +23,7 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className=''>
-                    <div className='mx-auto text-center px-0 lg:px-10 '>
+                    <div className='mx-auto text-center lg:px-10 '>
                         <p>"Managing my taxes as a freelancer used to be overwhelming, but Noble Finance made it effortless.”</p>
                         <div>
                             <p>Margaret L., 24 years old</p>
