8c020bbaa89b2bb52a18d4bfe3fc840077618a62
meune <ekpeterechioma@gmail.com>
2025-08-18 02:43:01 -0600

    New feature added

--- src/components/Custom.jsx
+++ src/components/Custom.jsx
@@ -3,8 +3,12 @@ import Img from '../assets/FinanceBrick.webp'
 
 const Custom = () => {
   return (
-    <div className='w-full'>
-
+    <div className='w-full h-3/4 flex flex-col justify-center items-center p-10'>
+        <div>
+            <div className=''>
+                <img src={Img} alt="Finance Brick" className='w-1/3 h-1/2 rounded-2xl' />
+            </div>
+        </div>
     </div>
   )
 }
