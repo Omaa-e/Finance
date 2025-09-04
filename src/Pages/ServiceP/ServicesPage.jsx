4994d6158b456983ef0b34379ad8dbe898518836
meune <ekpeterechioma@gmail.com>
2025-09-04 14:33:12 -0600

    Goodbye section done

--- src/Pages/ServiceP/ServicesPage.jsx
+++ src/Pages/ServiceP/ServicesPage.jsx
@@ -3,6 +3,8 @@ import bgImg from '../../assets/laptopCoin.avif'
 import { Link } from "react-router-dom";
 import Footer from '../../components/Footer';
 import Back from './Back';
+import Simple from './Simple';
+import Goodbye from './Goodbye';
 
 const ServicesPage = () => {
   return (
@@ -13,8 +15,8 @@ const ServicesPage = () => {
                 <img src={bgImg} className='w-full max-w-lg  md:max-w-lg lg:max-w-2xl'/>
                 </div>
                 <div className='flex flex-col justify-center order-2 md:order-1 md:w-[60%]'>
-                    <h2 className='text-3xl text-[#2e4f21] md:text-6xl w-[90%] mb-4 lg:w-3/4 lg:text-7xl font-normal tracking-tighter'>Your Financial Journey, Clearly Defined</h2>
-                    <p className='mb-6 md:w-[4/5] text-[#2e4f21]'>Trusted financial guidance for every stage of life and business since 1987</p>
+                    <h2 className='text-3xl text-[#2e4f21] md:text-6xl  mb-4 lg:w-3/4 lg:text-7xl font-normal tracking-tighter'>Your Financial Journey, Clearly Defined</h2>
+                    <p className='mb-15 md:w-[80%] text-[#2e4f21]'>Trusted financial guidance for every stage of life and business since 1987</p>
                     <div>
                         <Link to="/book">
                             <a className='bg-[#2e4f21] text-white text-xs hover:bg-green-300 hover:text-green-900 rounded-3xl py-3 px-3 transition'>Connect with our experts</a>
@@ -24,6 +26,8 @@ const ServicesPage = () => {
             </div>     
         </div>
         <Back/>
+        <Simple/>
+        <Goodbye/>
         <Footer/>
     </div>
   )
--- src/Pages/ServiceP/Simple.jsx
+++ src/Pages/ServiceP/Simple.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Img1 from '../../assets/pencil.webp'
 
-function Simple  () {
+function Simple () {
   return (
-    <div>
-        <div>
-            <div></div>
+    <div className='w-full h-auto py-20 px-15 '>
+        <div className='flex flex-col gap-10'>
+            <div className='text-[#2e4f21]'>
+                <p>How it works</p>
+                <h1 className='text-5xl py-6'>A simple onboarding</h1>
+                <p className=''>Serving individuals and small businesses since 1987</p>
+            </div>
+            <div className='flex flex-col xl:flex-row text-[#2e4f21] mt-15 xl:px-40'>
+                <h1 className='text-9xl font-extralight mb-8'>1</h1>
+                <div className='flex flex-col'>
+                    <h2 className='text-3xl'>Our tax advisors write you a plan</h2>
+                    <p>Accurately prepare and file personal and business tax returns to maximize deductions and ensure compliance.</p>
+                </div>
+                <div className=' bg-[#F4F7F5] rounded-2xl p-5 xl:mb-0 xl:w-25'>
+                    <img src={Img1} alt="" className='' />
+                </div>
+            </div>
         </div>
     </div>
   )
